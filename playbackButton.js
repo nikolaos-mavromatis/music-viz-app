2e0ce9b47cb76d93eab3fe6a9a356162620ec477
nikolaos-mavromatis <Nikolaos.Mavromatis@nike.com>
2022-06-09 14:43:06 +0200

    replaced w,h with r

--- playbackButton.js
+++ playbackButton.js
@@ -4,8 +4,7 @@
 function PlaybackButton() {
   this.x = width / 2;
   this.y = height - 80;
-  this.width = 30;
-  this.height = 30;
+  this.r = 30;
 
   //flag to determine whether to play or pause after button click and
   //to determine which icon to draw
@@ -23,7 +22,7 @@ function PlaybackButton() {
   //checks for clicks on the button, starts or pauses playabck.
   //@returns true if clicked false otherwise.
   this.hitCheck = function () {
-    if (dist(mouseX, mouseY, this.x, this.y) < this.width) {
+    if (dist(mouseX, mouseY, this.x, this.y) < this.r) {
       if (sound.isPlaying()) {
         sound.pause();
         noLoop();
@@ -40,21 +39,21 @@ function PlaybackButton() {
   this.buttonFrame = function () {
     fill('#24f17e');
     noStroke();
-    ellipse(this.x, this.y, 2 * this.width, 2 * this.width);
+    ellipse(this.x, this.y, 2 * this.r, 2 * this.r);
   }
 
   this.playButton = function () {
     fill('#FFFFFF');
     triangle(
-      this.x - this.width / 3, this.y - this.height / 2, // top left
-      this.x + this.width / 2, this.y, // right
-      this.x - this.width / 3, this.y + this.height / 2, // bottom left
+      this.x - this.r / 3, this.y - this.r / 2, // top left
+      this.x + this.r / 2, this.y, // right
+      this.x - this.r / 3, this.y + this.r / 2, // bottom left
     );
   }
 
   this.pauseButton = function () {
     fill('#FFFFFF');
-    rect(this.x - this.width / 2, this.y - this.height / 2, this.width / 2 - 2, this.height);
-    rect(this.x + 2, this.y - this.height / 2, this.width / 2 - 2, this.height);
+    rect(this.x - this.r / 2, this.y - this.r / 2, this.r / 2 - 2, this.r);
+    rect(this.x + 2, this.y - this.r / 2, this.r / 2 - 2, this.r);
   }
 }
