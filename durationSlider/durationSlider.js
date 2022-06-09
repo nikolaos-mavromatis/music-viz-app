c9aca69a357411c660d23f2ec0c99147850a4c3d
nikolaos-mavromatis <Nikolaos.Mavromatis@nike.com>
2022-06-09 16:48:32 +0200

    added progress bar and comments

--- durationSlider/durationSlider.js
+++ durationSlider/durationSlider.js
@@ -1,8 +1,13 @@
+// FIXME: when song is paused the slider returns to 0
+// FIXME: when moving to fullscreen the position is higher
+
 class DurationSlider {
-  // FIXME: when song is paused the slider returns to 0
-  // FIXME: when moving to fullscreen the position is higher
+  /**
+   * Displays a slider indicating the total duration of the track
+   * and the progress in playback time.
+   */
   constructor() {
-    this.x = 50;
+    this.x = 80;
     this.y = height - 20;
     this.w = width - 2 * this.x;
     this.h = 6;
@@ -15,10 +20,14 @@ class DurationSlider {
     strokeWeight(4);
     rect(this.x, this.y, this.w, this.h, 3);
 
-    //current time point
     let ctX = map(current, 0, duration, this.x, this.x + this.w);
+    // progress bar
     noStroke();
     fill('#24f17e');
+    rect(this.x - 2, this.y - 2, ctX - this.x, this.h + 4, 5);
+
+    //current time point
+    noStroke();
     ellipse(ctX, this.y + 3, 20);
   }
 }
