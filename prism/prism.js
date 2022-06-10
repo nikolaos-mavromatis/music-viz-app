ecbca3f5ed57758f465be246d5da0c553f0326de
nikolaos-mavromatis <Nikolaos.Mavromatis@nike.com>
2022-06-10 13:43:20 +0200

    basic wave functionality

--- prism/prism.js
+++ prism/prism.js
@@ -1,12 +1,9 @@
-var img;
-
-
 class Prism {
   constructor() {
     img.filter(BLUR, 2);
     this.name = "prism"
 
-    this.h = 200; //the height of the triangle
+    this.h = 300; //the height of the triangle
     this.b = 300; //the base of the triangle
     this.x = width / 2;
     this.y = height / 2;
@@ -16,29 +13,34 @@ class Prism {
   }
 
   draw() {
-    background(180);
-    // this.#drawStaticElements();
+    //load image
+    image(img, 0, 0, width, height);
+
+    stroke('black');
+    strokeWeight(25);
+    point(width / 2, height / 2);
+
 
     for (var i = this.waves.length - 1; i >= 0; i--) {
       var wave = this.waves[i];
+      wave.update();
+      wave.draw();
 
-      // // traverse backwards the waves array to delete wave if necessary
-      // if (!wave.outOfScreen) { // flag showing wave's radius is larger than half the 
-      //   wave.draw();
-      // } else {
-      //   this.waves.splice(i, 1);
-      //   this.waves.append(new Wave()); // 
+      //   // traverse backwards the waves array to delete wave if necessary
+      //   if (!wave.isOffScreen) { // flag showing wave's radius is larger than half the 
+      //     wave.draw();
+      //   } else {
+      //     this.waves.splice(i, 1);
+      //   }
     }
+    this.#drawTriangle();
   }
 
-  #drawStaticElements() {
-    // //load image
-    // image(img, 0, 0, width, height);
-
+  #drawTriangle() {
     //the 3 vertices of the triangle
-    let A = createVector(this.x, this.y - 100);
-    let B = createVector(this.x - 150, this.y + 100);
-    let C = createVector(this.x + 150, this.y + 100);
+    let A = createVector(this.x, this.y + this.h / 3 - 100);
+    let B = createVector(this.x - 150, this.y + this.h / 3 + 100);
+    let C = createVector(this.x + 150, this.y + this.h / 3 + 100);
 
     noStroke();
     fill('#fff')
@@ -47,7 +49,8 @@ class Prism {
     drawingContext.shadowBlur = 20;
     drawingContext.shadowColor = 'rgb(18, 16, 38)';
     fill('rgb(18, 16, 38)');
-    triangle(A.x, A.y + 10, B.x + 10, B.y - 5, C.x - 10, C.y - 5); //inner triangle
+    let offset = 10;
+    triangle(A.x, A.y + offset, B.x + offset, B.y - offset / 2, C.x - offset, C.y - offset / 2); //inner triangle
     drawingContext.shadowBlur = 0;
   }
 }
