1a3e36194e7601a933c5002a727d2e9b9c7cd107
nikolaos-mavromatis <Nikolaos.Mavromatis@nike.com>
2022-06-16 01:34:57 +0200

    added rotating beam

--- radar.js
+++ radar.js
@@ -1,14 +1,19 @@
+var beamAngle = 0;
+
 class Radar {
   constructor() {
     this.name = 'radar';
 
     this.pos = createVector(width / 2, height / 2);
     this.dia = 700;
+
+    this.beam = new Beam(this.dia / 2);
   }
 
   draw() {
     push();
     rectMode(CENTER);
+    angleMode(DEGREES);
     translate(this.pos.x, this.pos.y);
 
     // grid
@@ -23,12 +28,36 @@ class Radar {
       }
       else {
         noStroke();
-        fill('white');
+        fill(255, 150);
         rect(i * tickStep - this.dia / 2, 0, 1, 10);
         rect(0, i * tickStep - this.dia / 2, 10, 1);
       }
     }
 
+    // draw beam
+    this.beam.draw()
+
     pop();
+  }
+}
+
+class Beam {
+  /**
+   * Generates a rotating radar beam.
+   * @param {int, float} r - the length of the beam
+   */
+  constructor(r) {
+    this.r = r;
+
+    this.angle = 0;
+  }
+
+  draw() {
+    rotate(this.angle);
+    stroke('green');
+    strokeWeight(3);
+    noFill();
+    line(0, 0, 0, this.r);
+    this.angle += 0.5;
   }
 }
